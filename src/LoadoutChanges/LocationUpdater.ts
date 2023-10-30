3029adc58285ce42d777a1cb78ddcba91dafc24c
andrewgdewar <andrewgdewar@gmail.com>
2023-10-29 22:34:03 -0600

    2.0.1 fix time

--- src/LoadoutChanges/LocationUpdater.ts
+++ src/LoadoutChanges/LocationUpdater.ts
@@ -1,5 +1,4 @@
 import { StaticRouterModService } from "@spt-aki/services/mod/staticRouter/StaticRouterModService";
-import { WeatherController } from "@spt-aki/controllers/WeatherController"
 import { DependencyContainer } from 'tsyringe';
 import { globalValues } from './GlobalValues';
 import { WeatherGenerator } from "@spt-aki/generators/WeatherGenerator";
@@ -10,18 +9,28 @@ export const LocationUpdater = (
 ): undefined => {
     const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
     const weatherGenerator = container.resolve<WeatherGenerator>("WeatherGenerator");
-    const weatherController = container.resolve<WeatherController>("WeatherController");
 
     staticRouterModService.registerStaticRouter(`AlgorithmicLevelProgressionMapUpdater`, [{
         url: "/client/raid/configuration",
         action: (_url, info, _sessionId, output) => {
-            const dateTime = weatherController.getCurrentInRaidTime()
-            const date = weatherGenerator.getInRaidTime(dateTime)
-            const hours = info.timeVariant === "PAST" ? date.getHours() - 12 : date.getHours()
+            const time = weatherGenerator.calculateGameTime({ acceleration: 0, time: "", date: "" }).time
+
+            const hours = getTime(time, info.timeVariant === "PAST" ? 12 : 0);
+
             globalValues.setValuesForLocation(info.location.toLowerCase(), hours)
             return output
         }
     }], "aki");
 
     globalValues.config.debug && console.log("Algorthimic LevelProgression: Custom router AlgorithmicLevelProgressionMapUpdater Registered")
+}
+
+
+function getTime(time: string, hourDiff): number {
+    let [h, m] = time.split(':');
+    // console.log("minutes", m)
+    if (parseInt(h) == 0) {
+        return Number(h)
+    }
+    return Number(Math.abs(parseInt(h) - hourDiff))
 }
